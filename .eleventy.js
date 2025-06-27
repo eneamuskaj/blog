2011d9c938cefa205f108e0cc82d34cd444374c6
Enea Muskaj <eneamuskaj@gmail.com>
2025-06-27 16:10:04 -0500

    add dark mode

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("posts", (collection) => {
     return collection.getFilteredByGlob("src/posts/*.md").reverse();
+  });
+
+  // Add a date filter using Luxon
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("MMMM d, yyyy");
   });
 
   // Passthrough copy for assets
